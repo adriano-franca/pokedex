0141c4495cc474d735eb0c0a0fbd50e2825985e6
adrianofranca111 <adriano.franca@discente.ufma.br>
2023-02-12 00:34:42 -0300

    requisitando dados da pokeapi

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
-
+    <div v-for="(poke, index) in pokemons" :key="index">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1" />
+    </div>
   </div>
 </template>
 
 <script>
 
-//import Axios from 'axios'
+import axios from 'axios'
+import Pokemon from './components/Pokemon-I'
 
 export default {
   name: 'App',
-  
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  created: function(){
+    
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      console.log("Pokemons was caught");
+      this.pokemons = res.data.results;
+    })
+  },
+  components: {
+    Pokemon, 
+  }
 }
 </script>
 
---
+++ src/components/Pokemon-I.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ num }} {{ uppercase(name) }}</h1>
+        <small>{{ url }}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    },
+    computed: {
+        uppercase(value){
+            console.log(value)
+            var newName = value[0].toUpperCase() + value.slice(1);
+            return newName;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
