a128735183eff3bfe7403c350c7015395f15317e
adrianofranca111 <adriano.franca@discente.ufma.br>
2023-02-13 09:56:12 -0300

    colocando imagem no componente poke

--- src/components/Pokemon-I.vue
+++ src/components/Pokemon-I.vue
@@ -1,14 +1,51 @@
 <template>
-    <div>
-        <h1>{{ num }} {{ name.toUpperCase() }}</h1>
-        <small>{{ url }}</small>
+    <div id="pokemon">
     </div>
+            <div class="card">
+        <div class="card-image">
+            <figure>
+            <img :src="currentImg" alt="Placeholder image">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+            
+            <div class="media-content">
+                <p class="title is-4">{{ num }} - {{ name.toUpperCase() }}</p>
+                <p class="subtitle is-5">{{ pokemon.type }}</p>
+            </div>
+            </div>
+
+            <div class="content">
+            
+            </div>
+        </div>
+        </div>
 </template>
 
 <script>
 
 import axios from 'axios'
 export default {
+    created: function(){
+        axios.get(this.url).then(res => {
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+            this.currentImg = this.pokemon.front;
+        })
+    },
+    data(){
+        return {
+            isFront: true,
+            currentImg: '',
+            pokemon: {
+                type: '',
+                front: '', 
+                back: ''
+            }
+        }
+    },
     props: {
         num: Number,
         name: String,
@@ -18,5 +55,7 @@ export default {
 </script>
 
 <style>
-
+#pokemon {
+    margin-top:3%;
+}
 </style>
